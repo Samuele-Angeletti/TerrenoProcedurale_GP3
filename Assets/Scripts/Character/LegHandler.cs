8799622f1c458227bb1db0650785e79e2eaeb2c1
Noxianblade99 <novateam987@gmail.com>
2026-06-06 11:27:31 +0200

    Changed LegHandler
    Co-Authored-By: Samu Dev <104771865+Samuele-Angeletti@users.noreply.github.com>

--- Assets/Scripts/Character/LegHandler.cs
+++ Assets/Scripts/Character/LegHandler.cs
@@ -3,24 +3,98 @@
 [RequireComponent(typeof(GroundCheck))]
 public class LegHandler : MonoBehaviour
 {
-    [SerializeField] Transform targetFoot;
-    private GroundCheck groundCheck;
+    [SerializeField] LayerMask terrainLayer = default;
+    [SerializeField] Transform body = default;
+    [SerializeField] LegHandler otherFoot = default;
+    [SerializeField] float speed = 1;
+    [SerializeField] float stepDistance = 4;
+    [SerializeField] float stepLength = 4;
+    [SerializeField] float stepHeight = 1;
+    [SerializeField] Vector3 footOffset = default;
+    float footSpacing;
+    Vector3 oldPosition, currentPosition, newPosition;
+    Vector3 oldNormal, currentNormal, newNormal;
+    float lerp;
 
-    private void Awake()
+    private void Start()
     {
-        groundCheck = GetComponent<GroundCheck>();
+        footSpacing = transform.localPosition.x;
+        currentPosition = newPosition = oldPosition = transform.position;
+        currentNormal = newNormal = oldNormal = transform.up;
+        lerp = 1;
     }
 
-    private void Update()
-    {
-        SetFootToGround();
-    }
+    // Update is called once per frame
 
-    private void SetFootToGround()
+    void Update()
     {
-        if (groundCheck.GroundHit(out var position, out var normal))
+        transform.position = currentPosition;
+        transform.up = currentNormal;
+
+        Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
         {
-            targetFoot.SetPositionAndRotation(position, Quaternion.Euler(normal));
+            Debug.Log(Vector3.Distance(newPosition, info.point));
+            if (Vector3.Distance(newPosition, info.point) > stepDistance && !otherFoot.IsMoving() && lerp >= 1)
+            {
+                lerp = 0;
+                int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPosition).z ? 1 : -1;
+                newPosition = info.point + (body.forward * stepLength * direction) + footOffset;
+                newNormal = info.normal;
+            }
+        }
+
+        if (lerp < 1)
+        {
+            Vector3 tempPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            tempPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = tempPosition;
+            currentNormal = Vector3.Lerp(oldNormal, newNormal, lerp);
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+            oldNormal = newNormal;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newPosition, 0.5f);
+    }
+
+
+
+    public bool IsMoving()
+    {
+        return lerp < 1;
+    }
+
+    //[SerializeField] Transform targetFoot;
+    //private GroundCheck groundCheck;
+
+    //private void Awake()
+    //{
+    //    groundCheck = GetComponent<GroundCheck>();
+    //}
+
+    //private void Update()
+    //{
+    //    SetFootToGround();
+    //}
+
+    //private void SetFootToGround()
+    //{
+    //    if (groundCheck.GroundHit(out var position, out var normal))
+    //    {
+    //        targetFoot.SetPositionAndRotation(position, Quaternion.Euler(normal));
+    //    }
+    //}
+
+
 }
